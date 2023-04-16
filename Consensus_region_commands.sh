fe359519e37823751fb25162c05c190f99dedd15
kirtichhatlani <89626783+kirtichhatlani@users.noreply.github.com>
2023-04-16 17:31:31 -0400

    Update Consensus_region_commands.sh

--- Consensus_region_commands.sh
+++ Consensus_region_commands.sh
@@ -1,7 +1,13 @@
-ref='/storage/coda1/p-fstorici3/0/kchhatlani6/hu-mito/reference/hu-mito/filtered_hg38-chrM.fa'
-vcfpath='/storage/coda1/p-fstorici3/0/kchhatlani6/hu-mito/T8/Mutect'
+ref='/storage/coda1/p-fstorici3/0/kchhatlani6/hu-mito/reference/filtered_hg38-chrM.fa'
+vcfpath='/storage/coda1/p-fstorici3/0/kchhatlani6/hu-mito/vcfs'
+consensus_output='/storage/coda1/p-fstorici3/0/kchhatlani6/hu-mito/consensus'
+ND6_cordinates='/storage/coda1/p-fstorici3/0/kchhatlani6/hu-mito/ND6.bed'
 
-for sample in DLTB-8 TLTB-8 DLTB-P TLTB-P
-cat $ref | bcftools consensus $vcfpath/${sample}_tumoronly.vcf.gz > ${sample}.fa
-## test.bed has co-ordinates of ND6 region of mtDNA
-bedtools getfasta -fi ${sample}.fa -bed test.bed -fo ${sample}_ND6.fa
+for sample in DLTB-8 TLTB-8 DLTB-P TLTB-P; do
+cat $ref | bcftools consensus ${vcfpath}/${sample}/${sample}_tumoronly.vcf.gz > ${consensus_output}/${sample}/${sample}.fa
+done
+
+## ND6.bed has co-ordinates of ND6 region of mtDNA
+## chrM    4470    5511
+
+bedtools getfasta -fi ${consensus_output}/${sample}/${sample}.fa -bed ${ND6_cordinates} -fo ${consensus_output}/${sample}/${sample}_ND6.fa
