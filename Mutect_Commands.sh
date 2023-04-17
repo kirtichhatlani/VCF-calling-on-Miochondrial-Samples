1e628debd772adee74da729cfa1383770d1beab0
kirtichhatlani <89626783+kirtichhatlani@users.noreply.github.com>
2023-04-16 20:12:25 -0400

    Update Mutect_Commands.sh

--- Mutect_Commands.sh
+++ Mutect_Commands.sh
@@ -1,24 +1,11 @@
-ref='/storage/coda1/p-fstorici3/0/kchhatlani6/hu-mito/reference/hu-mito/filtered_hg38-chrM.fa'
-readpath='/storage/coda1/p-fstorici3/0/kchhatlani6/hu-mito/DNA-seq/trimmed/'
-dbsnp='/storage/coda1/p-fstorici3/0/kchhatlani6/hu-mito/dbSNP'
+ref='/storage/coda1/p-fstorici3/0/kchhatlani6/hu-mito/reference/filtered_hg38-chrM.fa'
+bam_output='/storage/coda1/p-fstorici3/0/kchhatlani6/hu-mito/bams'
+DB='/storage/coda1/p-fstorici3/0/kchhatlani6/DBs'
 
-# INEDXING REF GENOME
-bwa index $ref
-gatk CreateSequenceDictionary -R $ref
-
-# ALIGNMENT
-for sample in FS25-H8 FS25-T8 FS25-HP FS25-TP
+for sample in DLTB-8 TLTB-8 DLTB-P TLTB-P;
 do
-### mapping 2 reads with ref genome
-bwa mem -R '@RG\tID:foo\tSM:bar\tLB:library1' $ref $readpath/${sample}_R1_001_val_1.fq  $readpath/${sample}_S4_L001_R2_001_val_2.fq > ${sample}_lane.sam
-### sam to bam
-samtools fixmate -O bam ${sample}_lane.sam ${sample}_lane_fixmate.bam
-### sorting bam file
-samtools sort -O bam -o ${sample}_lane_sorted.bam ${sample}_lane_fixmate.bam
-### indexing bam file
-samtools index ${sample}_lane_sorted.bam
-### variant calling
-gatk Mutect2 -R $ref -I ${sample}_lane_sorted.bam --germline-resource af-only-gnomad.hg38.vcf.gz --panel-of-normals 1000g_pon.hg38.vcf.gz -O ${sample}_tumoronly.vcf.gz
+# variant calling
+gatk Mutect2 -R $ref -I ${bam_output}/${sample}_sorted.bam --germline-resource ${DB}/af-only-gnomad.hg38.vcf.gz --panel-of-normals ${DB}/1000g_pon.hg38.vcf.gz -O ${sample}_tumoronly.vcf.gz
 ### changing chrM to MT for annotation
 gunzip ${sample}_tumoronly.vcf.gz
 sed 's/chrM/MT/g' ${sample}_tumoronly.vcf > ${sample}_tumoronlyMT.vcf
